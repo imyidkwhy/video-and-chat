3e8d7091b681e46bb67ef70943da31ab05327714
imyidkwhy <imyidkwhy@gmail.com>
2025-02-11 07:09:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,37 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const io = require('socket.io')(server);
 
-// Создаем экземпляр приложения Express
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-
-// Статические файлы из папки 'public'
-app.use(express.static('public'));
-
-// Хранение пользователей
-const users = new Map();
-
-// Обработка подключения новых пользователей
 io.on('connection', (socket) => {
-  // Обработчик установки имени
-  socket.on('setName', (name) => {
-    users.set(socket.id, name || 'Неизвестный');
-    console.log(`Пользователь ${socket.id} установил имя: ${name}`);
-  });
+    console.log('A user connected: ' + socket.id);
 
-  // Обработчик сообщений
-  socket.on('message', (msg) => {
-    const userName = users.get(socket.id) || 'Неизвестный';
-    io.emit('message', {
-      name: userName,
-      text: msg.text,
+    socket.on('setName', (name) => {
+        users.set(socket.id, name || 'Неизвестный');
+        console.log(`Пользователь ${socket.id} установил имя: ${name}`);
     });
-  });
 
-  // Обработчик воспроизведения видео
-   socket.on('play', () => {
-        socket.broadcast.emit('play'); // Отправляем событие всем остальным пользователям
+    socket.on('message', (msg) => {
+        const userName = users.get(socket.id) || 'Неизвестный';
+        io.emit('message', {
+            name: userName,
+            text: msg.text,
+        });
+    });
+
+    socket.on('disconnect', () => {
+        users.delete(socket.id);
+    });
+
+    // Синхронизация воспроизведения
+    socket.on('play', () => {
+        socket.broadcast.emit('play');
     });
 
     socket.on('pause', () => {
-        socket.broadcast.emit('pause'); // Отправляем событие всем остальным пользователям
+        socket.broadcast.emit('pause');
     });
 
     socket.on('seek', (time) => {
-        socket.broadcast.emit('seek', time); // Отправляем событие всем остальным пользователям
+        socket.broadcast.emit('seek', time);
     });
-});
-
-  // Удаляем пользователя при отключении
-  socket.on('disconnect', () => {
-    users.delete(socket.id);
-  });
 });
 
 // Запуск сервера
