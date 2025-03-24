08e2209f97e7781726db2855c0d555a9374ac321
imyidkwhy <imyidkwhy@gmail.com>
2025-03-24 16:25:53 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 const socket = io()
 let userName = ''
 
-// Запрос имени при первом подключении
+
 while (!userName) {
   userName = prompt('Пожалуйста, введите ваше имя:')?.trim()
 }
 
-// Сохраняем имя и отправляем на сервер
+
 localStorage.setItem('chatName', userName)
 socket.emit('setName', userName)
 
-// Обработчик отправки сообщений
+
 document.getElementById('messageInput').addEventListener('keypress', (e) => {
   if (e.key === 'Enter' && e.target.value.trim()) {
     socket.emit('message', { text: e.target.value.trim() })
@@ -18,7 +18,7 @@ document.getElementById('messageInput').addEventListener('keypress', (e) => {
   }
 })
 
-// Обработчик получения сообщений
+
 socket.on('message', (data) => {
   const chatDiv = document.getElementById('chat')
   const messageElement = document.createElement('div')
@@ -40,22 +40,22 @@ inputs.forEach(input => {
 
 
 const videoElement = document.getElementById('video');
-// Отправляем событие при воспроизведении
+
 videoElement.addEventListener('play', () => {
     socket.emit('play');
 });
 
-// Отправляем событие при паузе
+
 videoElement.addEventListener('pause', () => {
     socket.emit('pause');
 });
 
-// Отправляем событие при перемотке
+
 videoElement.addEventListener('seeked', () => {
     socket.emit('seek', videoElement.currentTime);
 });
 
-// Получаем события от сервера и обновляем состояние видео
+
 socket.on('play', () => {
     videoElement.play();
 });
